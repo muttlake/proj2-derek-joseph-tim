2ed6addca348449b0affd390d38732928247a30a
timothy shepard <shepbop@gmail.com>
2018-04-09 13:28:12 -0400

    Adding Post, not working in database yet

---
+++ WeatherApp/WeatherApp.Library/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApp.Library
+{
+    public class Post
+    {
+        [Key]
+        public int PostID {get; set; }
+
+	public string BlogPost { get; set; }
+	public string ImageFile { get; set; }
+
+        public Post()
+        {
+        }
+
+
+	//Foreign Key for User
+	public int UserID { get; set; }
+	public User User { get; set; }
+
+    }
+}
