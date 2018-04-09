f72a2308e66ca7ba698a7533e339dc30fb18bc2a
joseph samson <samsonj3956@gmail.com>
2018-04-09 16:23:50 -0400

    Finish Register

---
+++ WeatherApp/WeatherApp.MVC/Controllers/RegisterController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Library;
+using WeatherApp.MVC.Models;
+
+namespace WeatherApp.MVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Console.WriteLine("test");
+            return View(new RegisterViewModel());
+        }
+
+         [HttpPost]
+        public IActionResult Index(RegisterViewModel model)
+        {
+
+            Console.WriteLine("post worked");
+            model.AddUser();
+            HttpContext.Session.Set<User>("User", model.usr);
+
+            return RedirectToAction("Index", "Landing");
+
+           
+        }
+    } 
+}
