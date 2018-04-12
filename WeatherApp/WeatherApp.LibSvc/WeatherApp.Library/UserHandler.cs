913e7fefc4c722d6e4e8506598dd18fe324d63ec
timothy shepard <shepbop@gmail.com>
2018-04-12 11:47:36 -0400

    Now Library service can communicate with data service

---
+++ WeatherApp/WeatherApp.LibSvc/WeatherApp.Library/UserHandler.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WeatherApp.Library
+{
+    public class UserHandler
+    {
+        private string _requestString = "http://localhost:9000/api/user";
+
+        public int UserID { get; set; }
+
+
+        public UserHandler(int id)
+        {
+            UserID = id;
+            _requestString += "/" + UserID.ToString();
+        }
+
+        public User GetUserFromDataSvc()
+        {
+            HttpWebRequest apiRequest = WebRequest.Create(_requestString) as HttpWebRequest;
+
+            string jsonResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                jsonResponse = reader.ReadToEnd();
+            }
+            Console.WriteLine(jsonResponse);
+
+            return JsonConvert.DeserializeObject<User>(jsonResponse);
+        }
+    }
+}
