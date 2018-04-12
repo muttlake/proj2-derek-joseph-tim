b7c515ef44e59146f3b71e232ea1c8e780181c5d
timothy shepard <shepbop@gmail.com>
2018-04-11 22:05:17 -0400

    Added Data Service solution, and got data service to work for simple query of User or Post

---
+++ WeatherApp/WeatherApp.DataSvc/WeatherApp.DataSvc/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.DataSvc.WeatherApp.DB;
+
+namespace WeatherApp.DataSvc.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        private static WeatherAppContext context;
+
+        public UserController(WeatherAppContext db)
+        {
+            context = db;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await Task.Run(() => { return context.Users.ToList(); });
+        }
+
+
+    }
+}
