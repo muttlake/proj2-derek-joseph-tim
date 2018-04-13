f07e6125a6c0775c4103a6416dfbb6b42de0b987
joseph samson <samsonj3956@gmail.com>
2018-04-12 23:12:46 -0400

    amazon s3 post attmept

---
+++ WeatherApp/WeatherApp.MVC/Controllers/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Library;
+
+namespace WeatherApp.MVC.Controllers
+{
+    public class ImageController : Controller
+    {
+        [HttpPost]
+        public IActionResult Add()
+        {
+           // AWSConfig aws = new AWSConfig();
+            AWSS3Operations s3 = AWSS3Operations();
+            
+            s3.WriteAnObject(AWSConfig.GetCredentials, "joeaws1", "test","image", )
+            return View();
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp.MVC/Models/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherApp.Library;
+
+namespace WeatherApp.MVC.Models
+{
+    public class PostViewModel
+    {
+        public Post post { get; set; }
+    }
+}
