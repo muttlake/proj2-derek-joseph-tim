3b045998c3bc5f546eb44d7a6e54c23ea850b350
timothy shepard <shepbop@gmail.com>
2018-04-14 22:50:26 -0400

    Added AddPost to MVC

--- WeatherApp/WeatherApp.ClientMVC/WeatherApp.ClientLib/Post.cs
+++ WeatherApp/WeatherApp.ClientMVC/WeatherApp.ClientLib/Post.cs
@@ -34,5 +34,22 @@ namespace WeatherApp.ClientLib
         [JsonProperty("publishDateTime")]
         public DateTime PublishDateTime { get; set; }
 
+        public Post()
+        {
+
+        }
+
+        public Post(string blogPost, string imageFile, int userID, int tempFahr, string weatherJson, string weatherType, int zip)
+        {
+            BlogPost = blogPost;
+            ImageFile = imageFile;
+            UserID = userID;
+            TempFahr = tempFahr;
+            WeatherJson = weatherJson;
+            WeatherType = weatherType;
+            ZipCode = zip;
+            PublishDateTime = System.DateTime.Now;
+        }
+
     }
 }
