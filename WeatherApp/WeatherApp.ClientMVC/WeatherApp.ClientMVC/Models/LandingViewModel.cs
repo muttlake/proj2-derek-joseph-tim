1121d4bcd3e1b81f1161e799ea34f898b5a6ae5a
joseph samson <samsonj3956@gmail.com>
2018-04-16 11:16:40 -0400

    pww in Landing Page

--- WeatherApp/WeatherApp.ClientMVC/WeatherApp.ClientMVC/Models/LandingViewModel.cs
+++ WeatherApp/WeatherApp.ClientMVC/WeatherApp.ClientMVC/Models/LandingViewModel.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WeatherApp.ClientLib;
+using Newtonsoft.Json;
 
 namespace WeatherApp.ClientMVC.Models
 {
+    public class PostWithWeather
+    {
+        public Post Post { get; set; }
+        public RootObject Weather { get; set; }
+    }
     public class LandingViewModel
     {
         public User User { get; set; }
         public List<Post> Posts { get; set; }
-
+        public List<PostWithWeather> PostsWithWeather { get; set; }
         public RootObject HomeZipRootObject { get; set; }
-
         public string WeatherIcon { get; set; }
 
         public LandingViewModel(User u)
@@ -23,6 +28,7 @@ namespace WeatherApp.ClientMVC.Models
             HomeZipRootObject = jr.GetRootObjectFromLibSvc();
             SetWeatherIcon();
             GetPosts();
+            PostsWithWeather = GetPostsWithWeather();
         }
 
         public void GetPosts()
@@ -47,5 +53,18 @@ namespace WeatherApp.ClientMVC.Models
         {
             WeatherIcon = "http://openweathermap.org/img/w/" + HomeZipRootObject.weather[0].icon + ".png";
         }
+
+        public List<PostWithWeather> GetPostsWithWeather()
+         {
+             var postsWithWeather = new List<PostWithWeather>();
+             foreach(var post in Posts)
+             {
+                 var p = new PostWithWeather();
+                 p.Post = post;
+                 p.Weather = JsonConvert.DeserializeObject<RootObject>(post.WeatherJson);
+
+             }
+             return postsWithWeather;
+         }
     }
 }
