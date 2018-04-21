d04f5e847a4bd10154c905ba1571dfab3db4bf76
derek white <derekbwhite80@gmail.com>
2018-04-20 21:15:39 -0400

    fix code smell: remove the '_requestString' field and declare it as a local variable

--- WeatherApp/WeatherApp.LibSvc/WeatherApp.Library/UserHandler.cs
+++ WeatherApp/WeatherApp.LibSvc/WeatherApp.Library/UserHandler.cs
@@ -9,53 +9,51 @@ using System.Threading.Tasks;
 
 namespace WeatherApp.Library
 {
-    public class UserHandler
+  public class UserHandler
+  {
+    public int UserID { get; set; }
+    
+    public UserHandler()
     {
-        private readonly string _requestString;
-
-        public int UserID { get; set; }
-
-        public UserHandler()
-        {
-            var ash = new AppSettingsHandler();
-            _requestString = ash.JsonObject.DatabasePath;
-        }
-
-        public UserHandler(int id)
-        {
-            UserID = id;
-            var ash = new AppSettingsHandler();
-            _requestString = ash.JsonObject.DatabasePath;
-        }
-
-        public async Task<List<User>> GetUserFromDataSvcAsync()
-        {
-
-            var client = new HttpClient();
-            var result = await client.GetAsync("http://52.15.149.129/DataSvc/api/user/" + UserID.ToString());
-
-            if (result.IsSuccessStatusCode)
-            {
-                var user =  JsonConvert.DeserializeObject<User>(await result.Content.ReadAsStringAsync());
-                return new List<User>() { user };
-            }
-            else
-                return null;
-        }
-
-        public async Task<List<User>> GetAllUsersFromDataSvcAsync()
-        {
-
-            var client = new HttpClient();
-            var result = await client.GetAsync("http://52.15.149.129/DataSvc/api/user");
-
-            if (result.IsSuccessStatusCode)
-            {
-                return JsonConvert.DeserializeObject<List<User>>(await result.Content.ReadAsStringAsync());
-            }
-            else
-                return null;
-
-        }
+      var ash = new AppSettingsHandler();
+      string _requestString = ash.JsonObject.DatabasePath;
     }
+
+    public UserHandler(int id)
+    {
+      UserID = id;
+      var ash = new AppSettingsHandler();
+      string _requestString = ash.JsonObject.DatabasePath;
+    }
+
+    public async Task<List<User>> GetUserFromDataSvcAsync()
+    {
+
+      var client = new HttpClient();
+      var result = await client.GetAsync("http://52.15.149.129/DataSvc/api/user/" + UserID.ToString());
+
+      if (result.IsSuccessStatusCode)
+      {
+        var user = JsonConvert.DeserializeObject<User>(await result.Content.ReadAsStringAsync());
+        return new List<User>() { user };
+      }
+      else
+        return null;
+    }
+
+    public async Task<List<User>> GetAllUsersFromDataSvcAsync()
+    {
+
+      var client = new HttpClient();
+      var result = await client.GetAsync("http://52.15.149.129/DataSvc/api/user");
+
+      if (result.IsSuccessStatusCode)
+      {
+        return JsonConvert.DeserializeObject<List<User>>(await result.Content.ReadAsStringAsync());
+      }
+      else
+        return null;
+
+    }
+  }
 }
