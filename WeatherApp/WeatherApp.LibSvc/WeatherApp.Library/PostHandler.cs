d8599d4379333f82319569e8f2b9e14f40562ee6
derek white <derekbwhite80@gmail.com>
2018-04-20 21:14:04 -0400

    fix code smell: remove the '_requestString' field and declare it as a local variable

--- WeatherApp/WeatherApp.LibSvc/WeatherApp.Library/PostHandler.cs
+++ WeatherApp/WeatherApp.LibSvc/WeatherApp.Library/PostHandler.cs
@@ -7,37 +7,34 @@ using System.Text;
 
 namespace WeatherApp.Library
 {
-    public class PostHandler
+  public class PostHandler
+  {
+    public int PostID { get; set; }
+
+    public PostHandler()
     {
-        //This is the url to the data service
-        private readonly string _requestString;
-
-        public int PostID { get; set; }
-
-        public PostHandler()
-        {
-            var ash = new AppSettingsHandler();
-            _requestString = ash.JsonObject.DatabasePath;
-        }
-
-        public PostHandler(int id)
-        {
-            PostID = id;
-            var ash = new AppSettingsHandler();
-            _requestString = ash.JsonObject.DatabasePath;
-        }
-
-        public List<Post> GetPostFromDataSvc()
-        {
-            var drh = new DataSvcRequestHandler();
-            return new List<Post>() { JsonConvert.DeserializeObject<Post>(drh.GetJsonResponse("http://52.15.149.129/DataSvc/api/post" + PostID.ToString()).GetAwaiter().GetResult()) };
-        }
-
-        public List<Post> GetAllPostsFromDataSvc()
-        {
-            var drh = new DataSvcRequestHandler();
-            return JsonConvert.DeserializeObject<List<Post>>(drh.GetJsonResponse("http://52.15.149.129/DataSvc/api/post").GetAwaiter().GetResult());
-        }
-
+      var ash = new AppSettingsHandler();
+      string _requestString = ash.JsonObject.DatabasePath;
     }
+
+    public PostHandler(int id)
+    {
+      PostID = id;
+      var ash = new AppSettingsHandler();
+      string _requestString = ash.JsonObject.DatabasePath;
+    }
+
+    public List<Post> GetPostFromDataSvc()
+    {
+      var drh = new DataSvcRequestHandler();
+      return new List<Post>() { JsonConvert.DeserializeObject<Post>(drh.GetJsonResponse("http://52.15.149.129/DataSvc/api/post" + PostID.ToString()).GetAwaiter().GetResult()) };
+    }
+
+    public List<Post> GetAllPostsFromDataSvc()
+    {
+      var drh = new DataSvcRequestHandler();
+      return JsonConvert.DeserializeObject<List<Post>>(drh.GetJsonResponse("http://52.15.149.129/DataSvc/api/post").GetAwaiter().GetResult());
+    }
+
+  }
 }
