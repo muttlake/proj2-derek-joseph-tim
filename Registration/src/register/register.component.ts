2792ccb6ec3677f90813fa77f8678db1d734c903
joseph samson <samsonj3956@gmail.com>
2018-04-22 20:06:14 -0400

    Registeration ng app

---
+++ Registration/src/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { UserService } from "../api/userservice";
+import {NgForm} from '@angular/forms';
+
+@Component({
+    selector: 'reg',
+    templateUrl: 'reg.component.html',
+    styleUrls: ['reg.component.css']
+})
+
+export class RegisterComponent{
+
+//user:User;
+submitted = false;
+client: UserService;
+user = new User();
+constructor(client: UserService){
+    
+    this.client = client;
+};
+
+
+
+  onSubmit(f: NgForm) { 
+      this.submitted = true;
+      this.client.create(this.user).subscribe((err)=>console.log(err));
+ }
+
+  
+}
+
+
